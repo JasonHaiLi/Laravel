9ab0b7f524ce1938e06acd3cd6f1898b3ca3a97f
Jason Li <1207543053@qq.com>
2018-11-12 06:23:16 +0000

    Initial commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Hello Laravel! - by Jason
                 </div>
 
                 <div class="links">
